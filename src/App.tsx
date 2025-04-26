8b1cfc5f33cbe4936f238ac058ba796e0b113245
Serhii <messileonl@gmail.com>
2025-04-26 11:37:07 +0200

    change to useDeferredValue add default trækprocent add beløbUdBetalt

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useMemo, useState,  } from "react";
+import { useDeferredValue, useState } from "react";
 
 function App() {
+    const defaultTrækprocent = "38";
+
+
     const [beløb, setBeløb] = useState("");
-    const [trækprocent, setTrækprocent] = useState("");
+    const [trækprocent, setTrækprocent] = useState(defaultTrækprocent);
     const [message, setMessage] = useState(false);
-    const beløbBetal = useMemo(() => {
-        const sum =
-            (+beløb - +beløb * 0.08) * (+trækprocent / 100) + +beløb * 0.08;
-        return Math.ceil(sum);
-    }, [beløb, trækprocent]);
+
+  const defertBeløb = useDeferredValue(beløb)
+
+    const sum =
+    (+defertBeløb - +defertBeløb * 0.08) * (+trækprocent / 100) + +defertBeløb * 0.08;
+
+    const beløbBetal = Math.ceil(sum)
+
+    const beløbUdBetalt = Math.ceil(+defertBeløb - beløbBetal)
 
     async function onCopyBtnClick() {
         try {
@@ -41,6 +48,29 @@ function App() {
                 />
                 %
             </div>
+            <div className="text-green-700">
+                Jeg få udbetalt på konto: {beløbUdBetalt} kr.
+            </div>
+            <div className="text-red-700">
+                Skal betale: {beløbBetal} kr.
+                <button
+                    onClick={onCopyBtnClick}
+                    className="relative text-black pl-2"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
+                        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
+                    </svg>
+
+                    <span className="absolute right-[-55px] top-1/2 -translate-y-1/2">{message && "Copied"}</span>
+                </button>
+            </div>
             <p>
                 Trækprocent can jeg se på
                 <a
@@ -62,26 +92,8 @@ function App() {
                 </a>
             </div>
 
-            <div className="text-red-700">
-                Skal betale: {beløbBetal} kr.
-                <button
-                    onClick={onCopyBtnClick}
-                    className="relative text-black pl-2"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                    >
-                        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
-                        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
-                    </svg>
-
-                    <span className="absolute right-[-55px] top-1/2 -translate-y-1/2">{message && "Copied"}</span>
-                </button>
-            </div>
+            
+            
         </main>
     );
 }
